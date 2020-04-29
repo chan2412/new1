7b4e0a1682edafc1303e775dba93a88db172e8cf
CHANDHRU R <44549194+chan2412@users.noreply.github.com>
2020-04-29 20:52:30 +0530

    Updates

---
+++ precache-manifest.65002fd45cc8c613c7ad3fa92dd241bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed476611fb317b7d66e29d51fcc9b373",
+    "url": "/new1/index.html"
+  },
+  {
+    "revision": "a5a4dbdcfb7499c0829e",
+    "url": "/new1/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d4f24a4638d44cf6e439",
+    "url": "/new1/static/js/2.dc4a22ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/new1/static/js/2.dc4a22ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5a4dbdcfb7499c0829e",
+    "url": "/new1/static/js/main.7395f781.chunk.js"
+  },
+  {
+    "revision": "bde9a0b8dfefb71d8d9c",
+    "url": "/new1/static/js/runtime-main.f6c76467.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/new1/static/media/logo.5d5d9eef.svg"
+  }
+]);
